889de3a84d2a42513ab594bf5eb8d983220789e6
Tonashiro <vinit.tonashiro@hotmail.com>
2025-03-03 14:41:32 -0300

    feat: create constants

---
+++ src/constants/index.ts
@@ -0,0 +1,4 @@
+import contractData from "../../hardhat/artifacts/contracts/Nestad.sol/Nestad.json";
+
+export const contractABI = contractData.abi;
+export const contractBytecode = contractData.bytecode;
