e648b73b4f804fd7b3b7ef181ee2d8d1a110fa71
Tonashiro <vinit.tonashiro@hotmail.com>
2025-03-03 14:40:46 -0300

    feat: create Divider component

---
+++ src/components/Divider/index.tsx
@@ -0,0 +1,27 @@
+import { cn } from "@/lib/utils";
+import { cva, VariantProps } from "class-variance-authority";
+import React from "react";
+
+const dividerVariants = cva("border border-gray-100", {
+  variants: {
+    variant: {
+      vertical: "w-[2px] h-full mx-2",
+      horizontal: "h-[2px] w-full my-2",
+    },
+  },
+});
+
+export interface IDivider
+  extends React.HTMLAttributes<HTMLDivElement>,
+    VariantProps<typeof dividerVariants> {}
+
+export const Divider = React.forwardRef<HTMLDivElement, IDivider>(
+  ({ className, variant, ...props }, ref) => (
+    <div
+      className={cn(dividerVariants({ variant, className }))}
+      ref={ref}
+      {...props}
+    />
+  )
+);
+Divider.displayName = "Divider";
